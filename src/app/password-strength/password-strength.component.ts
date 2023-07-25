b42b0bd4c73578f22d1a6eb9cc246edb807c8aee
Pavlo Kliazmin <pavlo@flipfam.app>
2023-07-25 16:12:27 +0300

    chore: changed component

--- src/app/password-strength/password-strength.component.ts
+++ src/app/password-strength/password-strength.component.ts
@@ -1,25 +1,68 @@
-import { Component } from '@angular/core';
-import {FormsModule} from "@angular/forms";
+import { Component, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-password-strength',
   templateUrl: './password-strength.component.html',
-  styleUrls: ['./password-strength.component.css']
+  styleUrls: ['./password-strength.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => PasswordStrengthComponent),
+      multi: true,
+    },
+  ],
 })
-export class PasswordStrengthComponent {
+export class PasswordStrengthComponent implements ControlValueAccessor {
   password: string = '';
   passwordStrength: string = '';
   showPassword: boolean = false;
+
+  private onChange: any = () => {};
+  private onTouch: any = () => {};
 
   updatePasswordStrength() {
     if (!this.password.length) {
       this.passwordStrength = '';
     } else if (this.password.length < 8) {
       this.passwordStrength = 'easy';
-    } else if (this.password.match(/[a-zA-Z]/) && this.password.match(/[0-9]/) && this.password.match(/[!@#$%^&*]/)) {
+    } else if (
+      this.password.match(/[a-zA-Z]/) &&
+      this.password.match(/[0-9]/) &&
+      this.password.match(/[!@#$%^&*]/)
+    ) {
       this.passwordStrength = 'strong';
     } else {
       this.passwordStrength = 'medium';
+    }
+    this.onChange(this.password);
+  }
+
+  toggleShowPassword() {
+    this.showPassword = !this.showPassword;
+  }
+
+  // ControlValueAccessor methods
+  writeValue(value: any): void {
+    if (value !== undefined) {
+      this.password = value;
+      this.updatePasswordStrength();
+    }
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouch = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+  }
+  onKeyDown(event: KeyboardEvent) {
+    if (event.keyCode === 32) {
+      event.preventDefault();
     }
   }
 }
